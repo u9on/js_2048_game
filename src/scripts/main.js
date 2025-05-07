59a387db40893e7ad47141db344b270353711e38
Jenya Golovko <u9on1210@gmail.com>
2025-05-07 17:29:51 +0300

    second fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,62 @@
 'use strict';
 
-// Uncomment the next lines to use your game instance in the browser
-// const Game = require('../modules/Game.class');
-// const game = new Game();
+const Game = require('../modules/Game.class');
+const game = new Game();
 
-// Write your code here
+const startButton = document.querySelector('.button');
+const scoreButton = document.querySelector('.game-score');
+const startMessage = document.querySelector('.message-start');
+const loseMessage = document.querySelector('.message-lose');
+
+startButton.addEventListener('click', () => {
+  if (startButton.getAttribute('class') === 'button start') {
+    startButton.setAttribute('class', 'button restart');
+    startButton.textContent = 'Restart';
+    startMessage.style = 'display: none';
+    game.start();
+  } else {
+    loseMessage.setAttribute('class', 'message message-lose hidden');
+    game.restart();
+  }
+
+  game.getState();
+  game.getStatus();
+  scoreButton.textContent = game.getScore();
+});
+
+document.addEventListener('keydown', (e) => {
+  if (game.getStatus() === 'playing') {
+    switch (e.key) {
+      case 'ArrowLeft':
+        game.moveLeft();
+
+        break;
+      case 'ArrowRight':
+        game.moveRight();
+
+        break;
+      case 'ArrowUp':
+        game.moveUp();
+
+        break;
+      case 'ArrowDown':
+        game.moveDown();
+
+        break;
+    }
+
+    game.getState();
+
+    if (game.getStatus() === 'win') {
+      document
+        .querySelector('.message-win')
+        .setAttribute('class', 'message message-win');
+    } else if (game.getStatus() === 'lose') {
+      document
+        .querySelector('.message-lose')
+        .setAttribute('class', 'message message-lose');
+    }
+
+    scoreButton.textContent = game.getScore();
+  }
+});
